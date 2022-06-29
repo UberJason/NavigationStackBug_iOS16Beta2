2b702ae8681aee8b92ab7d687d27d7dc1db51ad0
Jason Ji <uberjason@gmail.com>
2022-06-28 21:56:28 -0400

    Initial Commit

---
+++ NavigationStackBug_iOS16Beta2/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  NavigationStackBug_iOS16Beta2
+//
+//  Created by Jason Ji on 6/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationStackBug_iOS16Beta2/NavigationStackBug_iOS16Beta2App.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackBug_iOS16Beta2App.swift
+//  NavigationStackBug_iOS16Beta2
+//
+//  Created by Jason Ji on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackBug_iOS16Beta2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
